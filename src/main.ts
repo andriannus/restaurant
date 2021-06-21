5c8eba8c784c1f6021a5409ed16d99978bcb94d1
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-06-21 18:41:56 +0700

    chore: install and setup fontawesome

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
 import { createApp } from "vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faBars,
+  faHeart,
+  faHome,
+  faInfoCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 import { App, router } from "@/app";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+
+library.add(faBars, faHeart, faHome, faInfoCircle);
+
+app.use(router);
+app.component("x-icon", FontAwesomeIcon);
+app.mount("#app");
