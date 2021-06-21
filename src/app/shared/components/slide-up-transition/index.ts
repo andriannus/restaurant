fb7a9f0451556815100b94124d0f3de955a535ff
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-06-21 16:29:58 +0700

    feat: create slide-up transition component

---
+++ src/app/shared/components/slide-up-transition/index.ts
@@ -0,0 +1,1 @@
+export { default as XSlideUpTransition } from "./slide-up-transition.component.vue";
---
+++ src/app/shared/components/slide-up-transition/slide-up-transition.component.vue
@@ -0,0 +1,18 @@
+<template>
+  <transition
+    name="SlideUp"
+    @before-enter="onBeforeEnter"
+    @enter="onEnter"
+    @after-enter="onAfterEnter"
+    @enter-cancelled="onEnterCancelled"
+    @before-leave="onBeforeLeave"
+    @leave="onLeave"
+    @after-leave="onAfterLeave"
+    @leave-cancelled="onLeaveCancelled"
+  >
+    <slot></slot>
+  </transition>
+</template>
+
+<script lang="ts" src="./slide-up-transition.component.ts"></script>
+<style lang="scss" scoped src="./slide-up-transition.component.scss"></style>
