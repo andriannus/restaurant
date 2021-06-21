14628003d9fa24b8c5537815f45685dc3a28bebe
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-06-21 20:51:53 +0700

    improvement: adjust card component

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -2,4 +2,19 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "CardComponent",
+
+  props: {
+    hover: {
+      type: Boolean,
+      default: false,
+    },
+    link: {
+      type: Boolean,
+      default: false,
+    },
+    to: {
+      type: String,
+      default: null,
+    },
+  },
 });
--- src/app/shared/components/card/card.component.vue
+++ src/app/shared/components/card/card.component.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="Card">
+  <router-link v-if="link" class="Card" :class="{ 'is-hover': hover }" :to="to">
+    <div class="Card-media">
+      <slot name="media"></slot>
+    </div>
+
+    <div class="Card-text">
+      <slot name="text"></slot>
+    </div>
+  </router-link>
+
+  <div v-else class="Card" :class="{ 'is-hover': hover }">
     <div class="Card-media">
       <slot name="media"></slot>
     </div>
